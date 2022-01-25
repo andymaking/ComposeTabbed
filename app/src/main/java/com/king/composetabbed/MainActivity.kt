170ca0a71631ecf6815386feeda5a9e0d1e93264
andymaking <andymaking@gmail.com>
2022-01-25 15:06:02 +0100

    Initial commit

---
+++ app/src/main/java/com/king/composetabbed/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.king.composetabbed
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import com.king.composetabbed.ui.theme.ComposeTabbedTheme
+import com.king.composetabbed.ui.theme.Purple700
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalPagerApi
+    @ExperimentalMaterialApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@ExperimentalPagerApi
+@ExperimentalMaterialApi
+@Composable
+fun MainScreen() {
+    val tabs = listOf(
+        TabItem.Music,
+        TabItem.Movies,
+        TabItem.Books
+    )
+    val pagerState = rememberPagerState()
+    Scaffold(
+        topBar = { /* Add code later */},
+    ) {
+        /* Add code later */
+    }
+}
+
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+        backgroundColor = Blue,
+        contentColor = Color.White
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
+}
+
+@ExperimentalPagerApi
+@ExperimentalMaterialApi
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTabbedTheme {
+
+    }
+}
+
+@ExperimentalPagerApi
+@ExperimentalMaterialApi
+@Composable
+fun Tabs(tabs: List<TabItem>, pagerState: PagerState) {
+    val scope = rememberCoroutineScope()
+    // OR ScrollableTabRow()
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        backgroundColor = Purple700,
+        contentColor = Color.White,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+            )
+        }) {
+        tabs.forEachIndexed { index, tab ->
+            // OR Tab()
+            LeadingIconTab(
+                icon = { Icon(painter = painterResource(id = tab.icon), contentDescription = "") },
+                text = { Text(tab.title) },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                },
+            )
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@ExperimentalPagerApi
+@Preview(showBackground = true)
+@Composable
+fun TabsPreview() {
+    val tabs = listOf(
+        TabItem.Music,
+        TabItem.Movies,
+        TabItem.Books
+    )
+    val pagerState = rememberPagerState()
+    Tabs(tabs = tabs, pagerState = pagerState)
+}
